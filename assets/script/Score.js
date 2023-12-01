b193116c551602516904708d5d109fcbfbb180ef
Bernard <oyebenard@gmail.com>
2023-11-30 19:24:41 -0600

    Static Page complete

--- assets/script/Score.js
+++ assets/script/Score.js
@@ -2,6 +2,26 @@
 
 export default 'Score';
 
-export class Contact {
-    
+export class Score {
+    #data;
+    #hits;
+    #percentage;
+
+    constructor(data, hits, percentage) {
+        this.#data = data;
+        this.#hits = hits;
+        this.#percentage = percentage;
+    }
+
+    get data() {
+        return this.#data;
+    }
+
+    get hits() {
+        return this.#hits;
+    }
+
+    get percentage() {
+        return this.#percentage
+    }
 }
--- assets/script/app.js
+++ assets/script/app.js
@@ -2,4 +2,25 @@
 
 import { onEvent, select, selectAll, create, print } from "./utils.js";
 
-import { Score } from "./Contact.js";
+import { Score } from "./Score.js";
+
+const words = [
+    'dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building',
+    'population', 'weather', 'bottle', 'history', 'dream', 'character', 'money',
+    'absolute', 'discipline', 'machine', 'accurate', 'connection', 'rainbow',
+    'bicycle', 'eclipse', 'calculator', 'trouble', 'watermelon', 'developer',
+    'philosophy', 'database', 'periodic', 'capitalism', 'abominable',
+    'component', 'future', 'pasta', 'microwave', 'jungle', 'wallet', 'canada',
+    'coffee', 'beauty', 'agency', 'chocolate', 'eleven', 'technology', 'promise',
+    'alphabet', 'knowledge', 'magician', 'professor', 'triangle', 'earthquake',
+    'baseball', 'beyond', 'evolution', 'banana', 'perfume', 'computer',
+    'management', 'discovery', 'ambition', 'music', 'eagle', 'crown', 'chess',
+    'laptop', 'bedroom', 'delivery', 'enemy', 'button', 'superman', 'library',
+    'unboxing', 'bookstore', 'language', 'homework', 'fantastic', 'economy',
+    'interview', 'awesome', 'challenge', 'science', 'mystery', 'famous',
+    'league', 'memory', 'leather', 'planet', 'software', 'update', 'yellow',
+    'keyboard', 'window', 'beans', 'truck', 'sheep', 'band', 'level', 'hope',
+    'download', 'blue', 'actor', 'desk', 'watch', 'giraffe', 'brazil', 'mask',
+    'audio', 'school', 'detective', 'hero', 'progress', 'winter', 'passion',
+    'rebel', 'amber', 'jacket', 'article', 'paradox', 'social', 'resort', 'escape'
+];
